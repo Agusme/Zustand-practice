3ca0663895a0922307fa8c87fe97722ce1d66813
agusmena <agusmena60@mail.com>
2025-09-09 11:27:46 -0300

    se agrega ejemplo de tareas con zustand

--- src/components/ejemplos/tarea/TareaModal.tsx
+++ src/components/ejemplos/tarea/TareaModal.tsx
@@ -1,6 +1,45 @@
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useTaskStore } from "../../../store/TareaStore";
+import { useState } from "react";
 
 export default function TareaModal() {
+
+  const { taskList, addTask, deleteTask, toggleTask } = useTaskStore()
+
+  const [inputTask, setInputTask] = useState('')
+
+  const handleAddTask = () => {
+    if (inputTask.trim() !== "") {
+      addTask(inputTask);
+      setInputTask('')
+    }
+  }
   return (
-    <div>TareaModal</div>
-  )
+    <div>
+      <h2 className="mt-3 mb-6">Demo</h2>
+      <div className="bg-[#E84A94] p-6 rounded-xl shadow-lg max-w-sm mx-auto">
+        <label className="floating-label">
+
+          <input type="text" value={inputTask}
+            placeholder="Escribe la tarea" className="input input-md text-black" onChange={(e) => setInputTask(e.target.value)} onKeyDown={(e) => { if (e.key === 'Enter') { handleAddTask() } }} />
+        </label>
+        {taskList.map((task) => (
+          <div className="flex mt-4 items-center justify-between" key={task.id}>
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                 checked={task.completed} 
+                className="checkbox text-white border-white"
+                onChange={() => toggleTask(task.id)}
+              />
+              <p className={`ms-2 ${task.completed? 'line-through': ''}`}>{task.taskName} </p>
+            </div>
+            <button className="btn btn-ghost" onClick={() => deleteTask(task.id)}>
+              <FaRegTrashAlt className="text-sky-400" size={21} />
+            </button>
+          </div>
+        ))}
+      </div>
+
+    </div>)
 }
---
+++ src/store/TareaStore.ts
@@ -0,0 +1,33 @@
+import {create} from 'zustand';
+
+type Task={
+    id: string;
+    taskName:string;
+    completed: boolean;
+}
+
+type TaskState={
+    taskList: Task[];
+    addTask:(taskName:string)=>void;
+    deleteTask:(id:string)=>void;
+    toggleTask:(id:string)=>void;
+}
+
+export const useTaskStore= create<TaskState>((set)=>({
+    taskList:[],
+  addTask: (taskName) =>
+    set((state) => ({
+      taskList: [
+        ...state.taskList, 
+        { id: crypto.randomUUID(), taskName, completed: false },
+      ],
+    })),
+
+    deleteTask:(id)=>set((state)=> ({
+        taskList: state.taskList.filter((task)=> task.id !== id)
+    })),
+
+    toggleTask:(id)=> set((state)=>({
+        taskList: state.taskList.map((task)=> task.id === id ? {...task, completed:!task.completed}: task)
+    }))
+}))
